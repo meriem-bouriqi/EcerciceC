d0203ff3095239879c2f1c3e4d387041540cca4d
meriem bouriqi <meriembouriqi@gmail.com>
2022-07-31 14:02:26 +0100

    add new folder

---
+++ air-cercle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    const float Pi=3.14;
+    float R;
+    float Surface;
+printf("veuillez entrer la valeur du rayon :\n");
+scanf("%f",&rayon);
+Surface = R*R*Pi;
+printf("l'air du cercle est de :%f\n:",Surface);
+
+
+}
